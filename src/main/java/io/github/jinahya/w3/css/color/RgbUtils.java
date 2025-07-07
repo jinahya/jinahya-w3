815b4d87d66e92dafe05c70b5821af1c8cdd7774
Jin Kwon <onacit@gmail.com>
2025-07-07 18:44:25 +0900

    wip

--- src/main/java/io/github/jinahya/w3/css/color/RgbUtils.java
+++ src/main/java/io/github/jinahya/w3/css/color/RgbUtils.java
@@ -4,7 +4,133 @@ import java.nio.DoubleBuffer;
 import java.util.Objects;
 import java.util.function.DoubleFunction;
 
-final class RgbUtils {
+public final class RgbUtils {
+
+    private static void checkRgbToHslArguments(final double red, final double green, final double blue) {
+        if (red < RgbConstants.MIN_NORMALIZED_COMPONENT || red > RgbConstants.MAX_NORMALIZED_COMPONENT) {
+            throw new IllegalArgumentException("red: " + red);
+        }
+        if (green < RgbConstants.MIN_NORMALIZED_COMPONENT || green > RgbConstants.MAX_NORMALIZED_COMPONENT) {
+            throw new IllegalArgumentException("green: " + green);
+        }
+        if (blue < RgbConstants.MIN_NORMALIZED_COMPONENT || blue > RgbConstants.MAX_NORMALIZED_COMPONENT) {
+            throw new IllegalArgumentException("blue: " + blue);
+        }
+    }
+
+    /**
+     * Put HSL color components, converted from specified RGB color components, to the specified buffer, and returns the
+     * buffer.
+     *
+     * @param red   a value of {@code red} component between {@value RgbConstants#MIN_NORMALIZED_COMPONENT} and
+     *              {@value RgbConstants#MAX_NORMALIZED_COMPONENT}, both inclusive.
+     * @param green a value of {@code green} component between {@value RgbConstants#MIN_NORMALIZED_COMPONENT} and
+     *              {@value RgbConstants#MAX_NORMALIZED_COMPONENT}, both inclusive.
+     * @param blue  a value of {@code blue} component between {@value RgbConstants#MIN_NORMALIZED_COMPONENT} and
+     *              {@value RgbConstants#MAX_NORMALIZED_COMPONENT}, both inclusive.
+     * @return an array of {@code hue}({@code [0..360]}, {@code whiteness}({@code [0..100]}, and
+     * {@code blackness}{@code [0..100]}.
+     */
+    public static double[] rgbToHsl(final double red, final double green, final double blue) {
+        checkRgbToHslArguments(red, green, blue);
+        final var max = Math.max(Math.max(red, green), blue);
+        final var min = Math.min(Math.min(red, green), blue);
+        var h = Double.NaN;
+        var s = .0d;
+        var l = (min + max) / 2;
+        final var d = max - min;
+        final var epsilon = 1 / 100000.0d;
+        if (d != 0) {
+            s = (l == 0 || l == 1) ? 0 : (max - l) / Math.min(l, 1 - l);
+            if (max == red) {
+                h = (green - blue) / d + (green < blue ? 6 : 0);
+            } else if (max == green) {
+                h = (blue - red) / d + 2;
+            } else {
+                h = (red - green) / d + 4;
+            }
+            h = h * 60;
+        }
+        if (s < 0) {
+            h += 180;
+            s = Math.abs(s);
+        }
+        if (h >= 360) {
+            h -= 360;
+        }
+        if (s <= epsilon) {
+            h = Double.NaN;
+        }
+        return new double[] {h, s * 100, l * 100};
+    }
+
+    /**
+     * Puts HSL color components, converted from specified RGB color components, to the specified buffer, and returns
+     * the buffer.
+     *
+     * @param red    a value of {@code red} component between {@value RgbConstants#MIN_NORMALIZED_COMPONENT} and
+     *               {@value RgbConstants#MAX_NORMALIZED_COMPONENT}, both inclusive.
+     * @param green  a value of {@code green} component between {@value RgbConstants#MIN_NORMALIZED_COMPONENT} and
+     *               {@value RgbConstants#MAX_NORMALIZED_COMPONENT}, both inclusive.
+     * @param blue   a value of {@code blue} component between {@value RgbConstants#MIN_NORMALIZED_COMPONENT} and
+     *               {@value RgbConstants#MAX_NORMALIZED_COMPONENT}, both inclusive.
+     * @param buffer the function.
+     * @return given {@code buffer} put with {@code hue}({@code [0..360]}, {@code whiteness}({@code [0..100]}, and
+     * {@code blackness}{@code [0..100]}.
+     * @see DoubleBuffer#put(double)
+     */
+    @SuppressWarnings("unchecked")
+    public static <T extends DoubleBuffer> T rgbToHsl(final double red, final double green, final double blue,
+                                                      final T buffer) {
+        checkRgbToHslArguments(red, green, blue);
+        Objects.requireNonNull(buffer, "buffer is null");
+        return (T) buffer.put(rgbToHsl(red, green, blue));
+    }
+
+    /**
+     * Applies HSL color components, converted from specified RGB color components, to the specified function, and
+     * returns the result.
+     *
+     * @param red      a value of {@code red} component between {@value RgbConstants#MIN_NORMALIZED_COMPONENT} and
+     *                 {@value RgbConstants#MAX_NORMALIZED_COMPONENT}, both inclusive.
+     * @param green    a value of {@code green} component between {@value RgbConstants#MIN_NORMALIZED_COMPONENT} and
+     *                 {@value RgbConstants#MAX_NORMALIZED_COMPONENT}, both inclusive.
+     * @param blue     a value of {@code blue} component between {@value RgbConstants#MIN_NORMALIZED_COMPONENT} and
+     *                 {@value RgbConstants#MAX_NORMALIZED_COMPONENT}, both inclusive.
+     * @param function the function.
+     * @param <R>      result type parameter
+     * @return the result of the {@code function} applied, in currying, with {@code hue}({@code [0..360]},
+     * {@code whiteness}({@code [0..100]}, and {@code blackness}{@code [0..100]}.
+     */
+    // https://www.w3.org/TR/css-color-4/#rgb-to-hsl
+    public static <R> R rgbToHsl(final double red, final double green, final double blue,
+                                 final DoubleFunction< // h
+                                         ? extends DoubleFunction< // w
+                                                 ? extends DoubleFunction< // b
+                                                         ? extends R>>> function) {
+        checkRgbToHslArguments(red, green, blue);
+        Objects.requireNonNull(function, "function is null");
+        final var hsl = rgbToHsl(red, green, blue);
+        return function
+                .apply(hsl[0])
+                .apply(hsl[1])
+                .apply(hsl[2])
+                ;
+    }
+
+    // -----------------------------------------------------------------------------------------------------------------
+
+    private static void checkRgbToHwbArguments(final double red, final double green, final double blue) {
+        if (red < RgbConstants.MIN_NORMALIZED_COMPONENT || red > RgbConstants.MAX_NORMALIZED_COMPONENT) {
+            throw new IllegalArgumentException("red: " + red);
+        }
+        if (green < RgbConstants.MIN_NORMALIZED_COMPONENT || green > RgbConstants.MAX_NORMALIZED_COMPONENT) {
+            throw new IllegalArgumentException("green: " + green);
+        }
+        if (blue < RgbConstants.MIN_NORMALIZED_COMPONENT || blue > RgbConstants.MAX_NORMALIZED_COMPONENT) {
+            throw new IllegalArgumentException("blue: " + blue);
+        }
+    }
 
     /**
      * .
@@ -15,7 +141,7 @@ final class RgbUtils {
      * @return hue as degrees 0..360
      */
     // https://www.w3.org/TR/css-color-4/#rgb-to-hwb
-    static double rgbToHue(final double red, final double green, final double blue) {
+    private static double rgbToHue(final double red, final double green, final double blue) {
         assert red >= RgbConstants.MIN_NORMALIZED_COMPONENT;
         assert red <= RgbConstants.MAX_NORMALIZED_COMPONENT;
         assert green >= RgbConstants.MIN_NORMALIZED_COMPONENT;
@@ -24,7 +150,7 @@ final class RgbUtils {
         assert blue <= RgbConstants.MIN_NORMALIZED_COMPONENT;
         final var max = Math.max(red, Math.max(green, blue));
         final var min = Math.min(red, Math.min(green, blue));
-        double hue = .0d;
+        double hue = Double.NaN;
         final var d = max - min;
         if (d != 0) {
             if (max == red) {
@@ -47,6 +173,37 @@ final class RgbUtils {
      * Put HWB color components, converted from specified RGB color components, to the specified buffer, and returns the
      * buffer.
      *
+     * @param red   a value of {@code red} component between {@value RgbConstants#MIN_NORMALIZED_COMPONENT} and
+     *              {@value RgbConstants#MAX_NORMALIZED_COMPONENT}, both inclusive.
+     * @param green a value of {@code green} component between {@value RgbConstants#MIN_NORMALIZED_COMPONENT} and
+     *              {@value RgbConstants#MAX_NORMALIZED_COMPONENT}, both inclusive.
+     * @param blue  a value of {@code blue} component between {@value RgbConstants#MIN_NORMALIZED_COMPONENT} and
+     *              {@value RgbConstants#MAX_NORMALIZED_COMPONENT}, both inclusive.
+     * @return an array of {@code hue}({@code [0..360]}, {@code whiteness}({@code [0..100]}, and
+     * {@code blackness}{@code [0..100]}.
+     */
+    public static double[] rgbToHwb(final double red, final double green, final double blue) {
+        checkRgbToHwbArguments(red, green, blue);
+        final var epsilon = 1 / 100000.0d;
+        var hue = rgbToHue(red, green, blue);
+        final var white = Math.min(Math.min(red, green), blue);
+        final var black = 1 - Math.max(Math.max(red, green), blue);
+        if (white + black >= 1 - epsilon) {
+            hue = Double.NaN;
+        }
+        assert hue >= HwbConstants.MIN_HUE;
+        assert hue <= HwbConstants.MAX_HUE;
+        assert white >= HwbConstants.MIN_WHITENESS;
+        assert white <= HwbConstants.MAX_WHITENESS;
+        assert black >= HwbConstants.MIN_BLACKNESS;
+        assert black <= HwbConstants.MAX_BLACKNESS;
+        return new double[] {hue, white, black};
+    }
+
+    /**
+     * Put HWB color components, converted from specified RGB color components, to the specified buffer, and returns the
+     * buffer.
+     *
      * @param red    a value of {@code red} component between {@value RgbConstants#MIN_NORMALIZED_COMPONENT} and
      *               {@value RgbConstants#MAX_NORMALIZED_COMPONENT}, both inclusive.
      * @param green  a value of {@code green} component between {@value RgbConstants#MIN_NORMALIZED_COMPONENT} and
@@ -61,30 +218,9 @@ final class RgbUtils {
     @SuppressWarnings("unchecked")
     public static <T extends DoubleBuffer> T rgbToHwb(final double red, final double green, final double blue,
                                                       final T buffer) {
-        if (red < RgbConstants.MIN_NORMALIZED_COMPONENT || red > RgbConstants.MAX_NORMALIZED_COMPONENT) {
-            throw new IllegalArgumentException("red: " + red);
-        }
-        if (green < RgbConstants.MIN_NORMALIZED_COMPONENT || green > RgbConstants.MAX_NORMALIZED_COMPONENT) {
-            throw new IllegalArgumentException("green: " + green);
-        }
-        if (blue < RgbConstants.MIN_NORMALIZED_COMPONENT || blue > RgbConstants.MAX_NORMALIZED_COMPONENT) {
-            throw new IllegalArgumentException("blue: " + blue);
-        }
+        checkRgbToHwbArguments(red, green, blue);
         Objects.requireNonNull(buffer, "buffer is null");
-        final var epsilon = 1 / 100000.0d;
-        var hue = rgbToHue(red, green, blue);
-        final var white = Math.min(Math.min(red, green), blue);
-        final var black = 1 - Math.max(Math.max(red, green), blue);
-        if (white + black >= 1 - epsilon) {
-            hue = 0;
-        }
-        assert hue >= HwbConstants.MIN_HUE;
-        assert hue <= HwbConstants.MAX_HUE;
-        assert white >= HwbConstants.MIN_WHITENESS;
-        assert white <= HwbConstants.MAX_WHITENESS;
-        assert black >= HwbConstants.MIN_BLACKNESS;
-        assert black <= HwbConstants.MAX_BLACKNESS;
-        return (T) buffer.put(hue).put(white).put(black);
+        return (T) buffer.put(rgbToHwb(red, green, blue));
     }
 
     /**
@@ -108,54 +244,14 @@ final class RgbUtils {
                                          ? extends DoubleFunction< // w
                                                  ? extends DoubleFunction< // b
                                                          ? extends R>>> function) {
-//        final var epsilon = 1 / 100000.0d;
-//        var hue = rgbToHue(red, green, blue);
-//        final var white = Math.min(Math.min(red, green), blue);
-//        final var black = 1 - Math.max(Math.max(red, green), blue);
-//        if (white + black >= 1 - epsilon) {
-//            hue = 0;
-//        }
-//        assert hue >= HwbConstants.MIN_HUE;
-//        assert hue <= HwbConstants.MAX_HUE;
-//        assert white >= HwbConstants.MIN_WHITENESS;
-//        assert white <= HwbConstants.MAX_WHITENESS;
-//        assert black >= HwbConstants.MIN_BLACKNESS;
-//        assert black <= HwbConstants.MAX_BLACKNESS;
-//        return function.apply(hue).apply(white).apply(black);
+        checkRgbToHwbArguments(red, green, blue);
         Objects.requireNonNull(function, "function is null");
-        final var buffer = rgbToHwb(
-                red,
-                green,
-                blue,
-                DoubleBuffer.allocate(3)
-        );
+        final var hwb = rgbToHwb(red, green, blue);
         return function
-                .apply(buffer.get(0))
-                .apply(buffer.get(1))
-                .apply(buffer.get(2));
-    }
-
-    /**
-     * Return an array of HWB components, converted from specified RGB color components, to the specified function, and
-     * returns the result.
-     *
-     * @param red   a value of {@code red} component between {@value RgbConstants#MIN_NORMALIZED_COMPONENT} and
-     *              {@value RgbConstants#MAX_NORMALIZED_COMPONENT}, both inclusive.
-     * @param green a value of {@code green} component between {@value RgbConstants#MIN_NORMALIZED_COMPONENT} and
-     *              {@value RgbConstants#MAX_NORMALIZED_COMPONENT}, both inclusive.
-     * @param blue  a value of {@code blue} component between {@value RgbConstants#MIN_NORMALIZED_COMPONENT} and
-     *              {@value RgbConstants#MAX_NORMALIZED_COMPONENT}, both inclusive.
-     * @return an array of {@code hue}({@code [0..360]}, {@code whiteness}({@code [0..100]}, and
-     * {@code blackness}{@code [0..100]}.
-     */
-    // https://www.w3.org/TR/css-color-4/#rgb-to-hwb
-    public static double[] rgbToHwb(final double red, final double green, final double blue) {
-        return rgbToHwb(
-                red,
-                green,
-                blue,
-                DoubleBuffer.allocate(3)
-        ).array();
+                .apply(hwb[0])
+                .apply(hwb[1])
+                .apply(hwb[2])
+                ;
     }
 
     // -----------------------------------------------------------------------------------------------------------------
